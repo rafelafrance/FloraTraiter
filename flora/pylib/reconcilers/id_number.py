1bd1e6da59e7d14101b333ae3c8dc9ea0236d735
rafelafrance <raphael.lafrance@gmail.com>
2023-12-12 11:29:26 -0500

    Use more rules to reconcile the recordNumber reconciler

--- flora/pylib/reconcilers/id_number.py
+++ flora/pylib/reconcilers/id_number.py
@@ -6,15 +6,9 @@ from traiter.pylib.reconcilers.base import Base
 class IdNumber(Base):
     acc_lb = "dwc:accessionNumber"
     id_lb = "dwc:recordedByID"
-    num_lb = "dwc:recordNumber"
 
     acc_match = Base.get_aliases(acc_lb)
     id_match = Base.get_aliases(id_lb, "dwc:recordedById")
-    num_match = Base.get_aliases(
-        num_lb,
-        """
-        dwc:record dwc:recordId dwc:recordedNumber dwc:recordnumber""",
-    )
 
     @classmethod
     def reconcile(cls, _: dict[str, Any], other: dict[str, Any]) -> dict[str, str]:
@@ -25,8 +19,5 @@ class IdNumber(Base):
 
         if val := cls.search(other, cls.id_match):
             obj[cls.id_lb] = val
-
-        if val := cls.search(other, cls.num_match):
-            obj[cls.num_lb] = val
 
         return obj
