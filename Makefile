b165ad2eae5f84febe9dc6ab6e4e63548a63dee9
rafelafrance <raphael.lafrance@gmail.com>
2024-01-10 12:09:10 -0500

    Add new subtree for parsing PDFs

--- Makefile
+++ Makefile
@@ -37,9 +37,21 @@ setup_subtrees:
 	git checkout main
 	git subtree add -q --squash --prefix=traiter merging/traiter
 
+	git remote add -f pdf_parsers https://github.com/rafelafrance/pdf_parsers.git
+	git checkout -b upstream/parse pdf_parsers/main
+	git subtree split -q --squash --prefix=parse --annotate='[parse] ' --rejoin -b merging/parse
+	git checkout main
+	git subtree add -q --squash --prefix=parse merging/parse
+
 fetch_subtrees:
 	git checkout upstream/traiter
 	git pull traiter/master
 	git subtree split -q --squash --prefix=traiter --annotate='[traiter] ' --rejoin -b merging/traiter
 	git checkout main
 	git subtree merge -q --squash --prefix=traiter merging/traiter
+
+	git checkout upstream/parse
+	git pull pdf_parsers/main
+	git subtree split -q --squash --prefix=parse --annotate='[parse] ' --rejoin -b merging/parse
+	git checkout main
+	git subtree merge -q --squash --prefix=parse merging/parse
--- flora/_update_pyproject_toml.py
+++ flora/_update_pyproject_toml.py
@@ -1,42 +1,74 @@
 #!/usr/bin/env python3
 import argparse
 import textwrap
+from dataclasses import dataclass, field
 from pathlib import Path
+from urllib.request import urlopen
 
 import tomlkit
 
-ROOT = Path()
-MAKE_FILE = ROOT / "Makefile"
-NAME = "pyproject.toml"
-PYPROJECT_TOML = ROOT / NAME
+PYPROJECT_TOML = Path() / "pyproject.toml"
 
-SUB = 4  # Index of the subtree dir in the "git subtree pull" command
+
+@dataclass
+class Subtree:
+    prefix: str
+    url: str
+    repo: str
+    deps: set[str] = field(default_factory=set)
 
 
 def main():
     _args = parse_args()
+    subtrees = get_subtrees()
+    get_deps(subtrees)
+    deps = merge_deps(subtrees)
 
-    with PYPROJECT_TOML.open() as fp:
-        pyproject = tomlkit.load(fp)
-    deps = set(pyproject["project"]["dependencies"])
-
-    for tree in get_subtrees():
-        path = ROOT / tree / NAME
-        with path.open() as fp:
-            subproject = tomlkit.load(fp)
-        deps |= set(subproject["project"]["dependencies"])
+    with PYPROJECT_TOML.open() as f:
+        pyproject = tomlkit.load(f)
 
     pyproject["project"]["dependencies"] = tomlkit.array()
     for dep in sorted(deps):
         pyproject["project"]["dependencies"].add_line(dep)
 
-    with PYPROJECT_TOML.open("w") as fp:
-        tomlkit.dump(pyproject, fp)
+    with PYPROJECT_TOML.open("w") as f:
+        tomlkit.dump(pyproject, f)
 
 
-def get_subtrees() -> list[str]:
-    with MAKE_FILE.open() as f:
-        return [ln.split()[SUB] for ln in f.readlines() if ln.find("subtree pull") > -1]
+def merge_deps(subtrees: list[Subtree]) -> list[str]:
+    deps = {"tomlkit"}
+    for tree in subtrees:
+        deps |= tree.deps
+    return sorted(deps)
+
+
+def get_deps(subtrees: list[Subtree]) -> None:
+    for tree in subtrees:
+        settings = urlopen(tree.url).read().decode("utf-8")  # noqa: S310
+        project = tomlkit.loads(settings)
+        tree.deps = set(project["project"]["dependencies"])
+
+
+def get_subtrees() -> list[Subtree]:
+    subtrees = []
+
+    branch = 3  # Offset of word in checkout -b command holding the prefix
+    make_file = Path() / "Makefile"
+
+    with make_file.open() as f:
+        for ln in f.readlines():
+            if ln.find("remote add") > -1:
+                *_, repo, url = ln.split()
+                url = url.replace("github.com", "raw.githubusercontent.com")
+                url = url.removesuffix(".git") + "/main/pyproject.toml"
+
+            elif ln.find("git checkout -b") > -1:
+                prefix = ln.split()[branch]
+                prefix = prefix.removeprefix("upstream/")
+
+                subtrees.append(Subtree(prefix=prefix, url=url, repo=repo))
+
+    return subtrees
 
 
 def parse_args():
@@ -47,8 +79,8 @@ def parse_args():
             """
             Update pyproject.toml with dependencies from subtrees.
 
-            It scans pyproject.toml files in subtrees and adds their dependencies
-            into the root level pyproject.toml.
+            It finds the subtrees and downloads the pyproject.toml for each from github
+            and builds a combined dependencies section from all of the subtree versions.
             """,
         ),
     )
